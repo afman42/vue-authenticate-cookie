89826e3435ef3dd1100a20b669d4d4de0faac07f
afman42 <afifarman50@gmail.com>
2022-05-21 22:58:18 +0700

    Add HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,67 @@
 <template>
-  <div class="container" style="margin-top:10px;">
+  <div class="container" style="margin-top: 10px">
     <div class="columns">
       <div class="column">
-        AAA
+        <ul>
+          <li>The Timer: 180 seconds</li>
+        </ul>
       </div>
       <div class="column">
-        {{ user }}
+        <h3 class="subtitle">{{ user.email }}</h3>
+        <b-button type="is-danger is-light" @click="logout">Logout</b-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Cookies from 'js-cookie'
-import axios from 'axios';
-import router from '../router'
+import Cookies from "js-cookie";
+import axios from "axios";
+import router from "../router";
 export default {
-  name: 'HomeView',
-  data(){
+  name: "HomeView",
+  data() {
     return {
       user: {},
-    }
+    };
   },
-  created(){
-    console.log(this.fetchUser());
+  created() {
+    this.fetchUser();
   },
   methods: {
-    fetchUser(){
-    const token = Cookies.get('jwt');
-      axios.get('/protected', { headers: { Authorization: 'Bearer ' + token }, withCredentials: true }).then(res => {
-        this.user = res.data.user
-        console.log(res)
-      }).catch(e => {
-        // console.log(e)
-        if (e.response.status == 403) {
-          router.push('/login');
-          axios.post('/auth/logout');
-        }
-      })
-    }
-  }
-}
+    fetchUser() {
+      const token = Cookies.get("jwt");
+      axios
+        .get("/protected", {
+          headers: { Authorization: "Bearer " + token },
+          withCredentials: true,
+        })
+        .then((res) => {
+          this.user = res.data.user;
+          console.log(res);
+        })
+        .catch((e) => {
+          // console.log(e)
+          if (e.response.status == 403) {
+            router.push("/login");
+            this.$buefy.toast.open({
+              message: "Logout Successfully",
+              type: "is-success",
+            });
+          }
+        });
+    },
+    async logout() {
+      // Cookies.remove('jwt');
+      await axios.post("/auth/logout", { withCredentials: true }).then((r) => {
+        console.log(r)
+        router.push("/login");
+        this.$buefy.toast.open({
+          message: "Logout Successfully",
+          type: "is-success",
+        });
+      });
+    },
+  },
+};
 </script>
